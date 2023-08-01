fb750d1a6bf01c6c1d1fcb80ad084d4ef01d88e6
kad-ecoli <cz378@MWPF2YVW8W>
2023-08-01 15:38:45 -0400

    cif2chain

--- script/curate_pdb.pl
+++ script/curate_pdb.pl
@@ -5,7 +5,7 @@ use Cwd 'abs_path';
 my $bindir = dirname(abs_path(__FILE__));
 my $rootdir = dirname($bindir);
 
-#system("$bindir/cif2fasta $rootdir/pdb/data/structures/divided/mmCIF/ $rootdir/pdb/derived_data/nuc.list > $rootdir/pdb/derived_data/nuc.fasta");
+system("$bindir/cif2fasta $rootdir/pdb/data/structures/divided/mmCIF/ $rootdir/pdb/derived_data/nuc.list > $rootdir/pdb/derived_data/nuc.fasta");
 system("grep -PA1 --no-group-separator '\\tRNA\\t\\d{2,}' $rootdir/pdb/derived_data/nuc.fasta > $rootdir/pdb/derived_data/rna.fasta");
 system("grep -PA1 --no-group-separator '\\tRNA\\t\\d\$' $rootdir/pdb/derived_data/nuc.fasta > $rootdir/pdb/derived_data/fragment.fasta");
 
