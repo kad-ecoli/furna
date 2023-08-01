0c7a6b8484b9f4eb5cfb04b92185a26a90593674
kad-ecoli <cz378@MWPF2YVW8W>
2023-08-01 15:30:13 -0400

    subset goa

---
+++ script/curate_goa.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use File::Basename;
+use Cwd 'abs_path';
+my $bindir = dirname(abs_path(__FILE__));
+my $rootdir = dirname($bindir);
+
+print "curate GOA\n";
+system("mkdir -p $rootdir/goa/UNIPROT");
+system("zgrep '^RNAcentral' $rootdir/goa/UNIPROT/goa_uniprot_all.gaf.gz|gzip - > $rootdir/goa/goa_RNAcentral_all.gaf.gz");
+system("$bindir/obo2csv $rootdir/goa/go-basic.obo $rootdir/goa/is_a.csv $rootdir/goa/name.csv $rootdir/goa/alt_id.csv");
+system("zcat $rootdir/goa/goa_RNAcentral_all.gaf.gz | $bindir/subset_GOA - $rootdir/RNAcentral/pdb.list $rootdir/goa/goa_RNAcentral_subset.gaf"); 
+exit();
