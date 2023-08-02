348397f60f847a07cad036c12b1ea23b0b768e7d
kad-ecoli <cz378@MWPF2YVW8W>
2023-08-02 13:49:51 -0400

    rfam

--- script/curate_rfam.pl
+++ script/curate_rfam.pl
@@ -8,7 +8,14 @@ my $rootdir = dirname($bindir);
 print "curate Rfam\n";
 system("mkdir -p $rootdir/Rfam");
 system("rm $rootdir/Rfam/rna_nr.split.*");
-system("split -l 1000 $rootdir/data/rna_nr.fasta $rootdir/Rfam/rna_nr.split.");
+system("zcat $rootdir/Rfam/Rfam.pdb.gz | cut -f2,3|sed 's/\t//g'|sort|uniq | $bindir/fasta2miss - $rootdir/data/rna.fasta - $rootdir/Rfam/Rfam.pdb.fasta");
+system("cat $rootdir/data/rna.fasta|grep '>'|sed 's/>//g' | $bindir/fasta2miss - $rootdir/Rfam/Rfam.pdb.fasta $rootdir/Rfam/Rfam.pdb.miss.list - > /dev/null");
+system("$bindir/fasta2len.py $rootdir/data/rna.fasta|grep -P '\\t\\d{1,3}\$' |cut -f1 >> $rootdir/Rfam/Rfam.pdb.miss.list");
+system("cat $rootdir/Rfam/Rfam.pdb.miss.list|sort|uniq | $bindir/fasta2miss - $rootdir/data/rna.fasta - $rootdir/Rfam/rna.fasta");
+system("$bindir/fasta_nr.py $rootdir/Rfam/rna.fasta $rootdir/Rfam/rna_nr.fasta $rootdir/Rfam/rna_nr.tsv");
+system("rm $rootdir/Rfam/rna.fasta  $rootdir/Rfam/Rfam.pdb.miss.list  $rootdir/Rfam/Rfam.pdb.fasta");
+system("split -l 1024 $rootdir/Rfam/rna_nr.fasta $rootdir/Rfam/rna_nr.split.");
+
 system("ls $rootdir/Rfam/|grep rna_nr.split. |cut -f3 -d.|sort|uniq > $rootdir/Rfam/rna_nr.list");
 foreach my $batch (`tac $rootdir/Rfam/rna_nr.list`)
 {
