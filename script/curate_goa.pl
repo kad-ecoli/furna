f0c584c34c8f41626cc1470febfde197e247999e
kad-ecoli <cz378@MWPF2YVW8W>
2023-08-10 13:54:26 -0400

    go2name

--- script/curate_goa.pl
+++ script/curate_goa.pl
@@ -10,4 +10,5 @@ system("mkdir -p $rootdir/goa/UNIPROT");
 system("zgrep '^RNAcentral' $rootdir/goa/UNIPROT/goa_uniprot_all.gaf.gz|gzip - > $rootdir/goa/goa_RNAcentral_all.gaf.gz");
 system("$bindir/obo2csv $rootdir/goa/go-basic.obo $rootdir/goa/is_a.csv $rootdir/goa/name.csv $rootdir/goa/alt_id.csv");
 system("zcat $rootdir/goa/goa_RNAcentral_all.gaf.gz | $bindir/subset_GOA - $rootdir/RNAcentral/pdb.list $rootdir/goa/goa_RNAcentral_subset.gaf"); 
+system("cat $rootdir/goa/name.csv |gzip - > $rootdir/data/go2name.tsv.gz");
 exit();
