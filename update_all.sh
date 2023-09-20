323d9321fdee2459cfe7bd551b6a2e907fe5eec0
kad-ecoli <cz378@MWPF2YVW8W>
2023-09-20 19:51:35 -0400

    update

---
+++ update_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+### 1. Download raw data ###
+script/download_pdb.pl
+script/download_rfam.pl
+script/download_rnacentral.pl
+script/download_goa.pl
+script/download_sifts.pl
+
+### 2. Curate data ###
+script/curate_pdb.pl
+script/curate_rfam.pl
+script/curate_rnacentral.pl
+script/curate_goa.pl
+script/combine_chain.pl
+
+### 3. Download data for each rna entry ###
+script/download_pubmed.pl
+script/download_assembly.pl
+script/combine_interaction.pl
+script/download_ligand.pl
+script/combine_protein.pl
+script/download_taxon.pl
+script/fasta2taxon.pl
+script/plot_goa.pl
+script/make_summary.pl
+script/combine_dna.pl
+script/curate_blastdb.pl
+script/download_uniprot.pl
+script/download_attract.pl
